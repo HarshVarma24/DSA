940c12de69d6d7548c42f8b51f006cdb16456378
HarshVarma24 <149789696+HarshVarma24@users.noreply.github.com>
2026-03-19 23:37:24 +0530

    Time: 53 ms (41.06%) | Memory: 46.5 MB (64.37%) - LeetSync

---
+++ 1129-longest-string-chain/longest-string-chain.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int longestStrChain(String[] words) {
+        int n = words.length;
+        int[] dp = new int[n];
+
+        Arrays.sort(words,(a, b)-> a.length() - b.length());
+        Arrays.fill(dp, 1);
+
+        int ans = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < i; j++){
+                if(checkPossible(words[i], words[j]) && dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+            ans = Math.max(dp[i], ans);
+
+        }
+        return ans;
+    }
+
+    public boolean checkPossible(String s1, String s2){
+
+        if(s1.length() != s2.length() + 1) return false;
+        int first = 0;
+        int second = 0;
+
+        while(first < s1.length()){
+            if(second < s2.length() && s1.charAt(first) == s2.charAt(second)){
+                first++;
+                second++;
+            }
+            else{
+                first++;
+            }
+        }
+        if(first == s1.length() && second == s2.length()) return true;
+        return false;
+    }
+}
