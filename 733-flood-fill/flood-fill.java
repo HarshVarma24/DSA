e0042a2da82bf27566018ef205fb52c5a5b337fe
HarshVarma24 <149789696+HarshVarma24@users.noreply.github.com>
2026-04-18 00:06:51 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (65.97%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,16 +1,16 @@
 class Solution {
     int rows;
     int columns;
-    void dfs(int row, int col, int newColor, int curColor, boolean[][] visited, int[][] image){
+    void dfs(int row, int col, int newColor, int curColor, int[][] image){
         if(row<0 || row >= rows || col < 0 || col >= columns) return;
-        if(image[row][col] != curColor) return;
-        if(visited[row][col] == true) return;
+        if( image[row][col] != curColor|| image[row][col] == newColor) return;
+        //if(visited[row][col] == true) return;
         else{
             image[row][col] = newColor;
-            visited[row][col] = true;
+            //visited[row][col] = true;
             int[][] adjList = {{row-1, col}, {row, col+1}, {row+1, col}, {row, col-1}};
             for(int neighbor[] : adjList){
-                dfs(neighbor[0], neighbor[1], newColor, curColor, visited, image);
+                dfs(neighbor[0], neighbor[1], newColor, curColor, image);
             }
         }
     }
@@ -18,8 +18,8 @@ class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
        rows = image.length;
        columns = image[0].length;
-       boolean[][] visited = new boolean[rows][columns];
-       dfs(sr, sc, color, image[sr][sc], visited, image);
+       //boolean[][] visited = new boolean[rows][columns];
+       dfs(sr, sc, color, image[sr][sc], image);
        return image;
 
     }
